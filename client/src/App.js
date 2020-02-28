61332233211385064671ee4e609abfdaaf67d6c6
Maksim <max.krylov@gmail.com>
2020-02-28 17:37:39 +0300

    React part ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Fib from './Fib';
 
 function App() {
   return (
+	  <Router>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,8 +22,15 @@ function App() {
         >
           Learn React
         </a>
+	  <Link to="/">Home</Link>
+	  <Link to="/otherpage">Other Pagae</Link>
       </header>
+	  <div>
+	  <Route exact path="/" component={Fib} />
+	  <Route path="/otherpage" component={OtherPage} />
     </div>
+	  
+	  </Router>
   );
 }
 
